86361a001f5acfb5f9581d75bcf8c4e47c78fd0d
haidm <hai.manh.dinh@gmail.com>
2025-12-24 02:46:11 +0700

    remove sam

--- TDDet/ultralytics/models/__init__.py
+++ TDDet/ultralytics/models/__init__.py
@@ -1,7 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 from .rtdetr import RTDETR
-from .sam import SAM
 from .yolo import YOLO
 
-__all__ = "YOLO", "RTDETR", "SAM"  # allow simpler import
+__all__ = "YOLO", "RTDETR" # allow simpler import
