4ea33aac4047dbd7e44d971d9c326cc5a4160890
haidm <hai.manh.dinh@gmail.com>
2025-12-23 11:08:16 +0700

    update yolo

---
+++ setup env/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+sudo apt install software-properties-common
+
+# Add deadsnakes PPA
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+# Install Python 3.9 and required packages
+sudo apt install python3.9 python3.9-dev python3.9-venv
+
+# Create virtual environment
+python3 -m venv yolov8_env
+
+# Activate virtual environment
+source yolov8_env/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
