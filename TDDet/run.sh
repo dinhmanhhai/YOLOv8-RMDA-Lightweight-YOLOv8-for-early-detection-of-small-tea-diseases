17e27cd054685f73ba3ee9de2391425d6ac09216
haidm <hai.manh.dinh@gmail.com>
2025-12-29 09:00:26 +0700

    update sh train

--- TDDet/run.sh
+++ TDDet/run.sh
@@ -5,5 +5,5 @@ export WANDB_ENTITY="works-haidinh-ptit"
 export WANDB_PROJECT="tddet_mqa"
 export WANDB_API_KEY="654322757bc621b514dc2592badff0c6eeefe6ad"
 
-# Chạy train
-python train.py
+# Chạy main
+python main.py
